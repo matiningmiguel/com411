fdc22d7d019e45de89c0ad96b095e90c18585e1c
Miguel Matining <5matim19@solent.ac.uk>
2021-09-30 10:26:54 +0100

    Added code to display an ascii art robot

---
+++ basics/output/ascii-art.py
@@ -0,0 +1,10 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+print("  ######  ")
+print("  #    #  ")
+print("  #    #  ")
+print("  #    #  ")
+print("  ######  ")
