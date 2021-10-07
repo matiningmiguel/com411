8b324ff9b523f12958dd2a25fa3fe7e0ba3a4274
unknown <5matim19@solent.ac.uk>
2021-10-07 10:16:27 +0100

    Aded if

---
+++ basics/decisions/simple decision.py
@@ -0,0 +1,7 @@
+# what type of book is beep reading
+print ("what is your favourite book genre?")
+book = input()
+if book == "adventure" :
+    print("I like adventure books!")
+
+print("Finished reading book!")
